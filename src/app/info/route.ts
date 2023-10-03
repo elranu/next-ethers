90cd13c24817609b1e2d112dadb2155aae81e5ef
Mariano Julio Vicario <elranu@gmail.com>
2023-10-03 18:53:26 -0300

    readme

--- src/app/info/route.ts
+++ src/app/info/route.ts
@@ -5,5 +5,4 @@ export async function GET(request: Request, res: Response) {
     const provider = new JsonRpcProvider("https://sepolia.infura.io/v3/8a30a48106eb413bb29d9ff89d0b99a6");
     const balance = await provider.getBalance(addr);
     return Response.json({ address: addr, balance });
-    return Response.json({ message: 'Hello world'  });
 }
