98d367c1ed90a19431022073184a8050d86587eb
Mariano Julio Vicario <elranu@gmail.com>
2023-10-03 18:29:29 -0300

    Initial commit

---
+++ src/app/info/route.ts
@@ -0,0 +1,9 @@
+import { JsonRpcProvider } from 'ethers';
+export async function GET(request: Request, res: Response) {
+   
+    const addr = "0xe7d66e4bde20e2e5f4f82bb99229abcdfa32c812"
+    const provider = new JsonRpcProvider("https://sepolia.infura.io/v3/8a30a48106eb413bb29d9ff89d0b99a6");
+    const balance = await provider.getBalance(addr);
+    return Response.json({ address: addr, balance });
+    return Response.json({ message: 'Hello world'  });
+}
